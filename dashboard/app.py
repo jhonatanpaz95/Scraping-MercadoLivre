2c87e90cc36a7d7314a7214e3893424f0d7c07f8
jhonatanpaz95 <jhonatanpaaz@gmail.com>
2024-09-25 09:43:25 -0300

    conclusão do projeto

--- dashboard/app.py
+++ dashboard/app.py
@@ -25,11 +25,11 @@ col1.metric(label='Numero total de Itens', value=total_itens)
 
 # KPI 2: numero total de itens
 unique_brands = df['brand'].nunique()
-col2.metric(abel='Numero de Marcas Únicas', value=unique_brands)
+col2.metric(label='Numero de Marcas Únicas', value=unique_brands)
 
 # KPI 3: numero total de itens
 average_new_price = df['new_price'].mean()
-col3.metric(abel='Preço Médio Novo (R$)', value=f'{average_new_price:.2f}')
+col3.metric(label='Preço Médio Novo (R$)', value=f'{average_new_price:.2f}')
 
 # Quais marcas são mais encontradas até a pagina 10
 st.subheader('Quais marcas são mais encontradas até a 10ª página')
@@ -49,6 +49,6 @@ col2.write(average_price_by_brand)
 st.subheader('Satisfação por marca')
 col1, col2 = st.columns([4,2])
 df_non_zero_reviews = df[df['reviews_rating_number'] > 0] 
-satisfaction_by_brand = df_non_zero_reviews.groupby('brand')['reviews_rating_number'].sort_values(ascending=False)
+satisfaction_by_brand = df_non_zero_reviews.groupby('brand')['reviews_rating_number'].mean().sort_values(ascending=False)
 col1.bar_chart(satisfaction_by_brand)
 col2.write(satisfaction_by_brand)
